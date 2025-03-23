4bf9c7355ea4e72b38c8ff86f6b44a275351ee56
Aashish Acharya <er.aashishacharya@gmail.com>
2025-03-23 17:17:16 +0545

    Third commit

--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -2,5 +2,5 @@ export default async function ProductDetails({params}:{
     params : Promise<{productId: string}>;
 }){
     const productId = (await params). productId;
-    return <h1>Details about product {productId}</h1>
+    return <h1>Details about product 0+{productId}</h1>
 }
